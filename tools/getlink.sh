8304d59df98ee00047169550e5915f36e8bc3ee6
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-04-19 19:03:41 +0200

    added getlink.sh

---
+++ tools/getlink.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Make link to serve html files as html files straight from github
+#
+# sverre.stikbakke@ntnu.no 19.04.2016
+#
+
+GITHUBUSER='sverres'
+
+cd ..
+REPO=`pwd`
+cd tools
+
+git log -1 --pretty=format:\
+"https://cdn.rawgit.com/$GITHUBUSER/`basename $REPO`/%H/"
