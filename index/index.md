0122068f71712385dfc2785635cbc1a15764d168
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-04-19 11:56:36 +0200

    Added rawgit link generation on index.html

--- index/index.md
+++ index/index.md
@@ -1,12 +1,26 @@
 # All files
+
 ## Plans
+
 - [week-1](./week-1.html)
+
 ## Presentations
+
 - [inspire](./inspire.html)
+
 ## Notes
+
 - [mdpublish](./mdpublish.html)
 - [note-1](./note-1.html)
 - [note-2](./note-2.html)
 - [note-3](./note-3.html)
 - [note-4](./note-4.html)
 - [README](./README.html)
+
+## Versions
+
+- [2016-04-18 19:31:11 +0200 |sverres |changed stylesheet for code to Prism Coy](https://cdn.rawgit.com/sverres/mdpublish/2c2310e61385a547982995ca95b59b79ed1e9dc9/)
+- [2016-04-18 18:34:54 +0200 |sverres |added mdpublish.md - in norwegian](https://cdn.rawgit.com/sverres/mdpublish/44695ec981d97c6f2bf47a5113edcbb475426256/)
+- [2016-04-18 14:46:37 +0200 |Sverre Stikbakke |files added](https://cdn.rawgit.com/sverres/mdpublish/15bdce8cd6c501b1123612716b651dd33798ae3f/)
+- [2016-04-18 14:42:22 +0200 |Sverre Stikbakke |first commit](https://cdn.rawgit.com/sverres/mdpublish/2e799c725c15a58eb0820f37ce1fcad99da9f7b1/)
+- [bleeding edge a.k.a. master](https://rawgit.com/sverres/mdpublish/master/)
--- tools/compileindex.sh
+++ tools/compileindex.sh
@@ -2,8 +2,10 @@
 #
 # make index-file with all markdown-files
 #
-# sverre.stikbakke@ntnu.no 18.04.2016
+# sverre.stikbakke@ntnu.no 19.04.2016
 #
+
+GITHUBUSER='sverres'
 
 PLANSFILES='../plans/*.md'
 NOTESFILES='../notes/*.md'
@@ -12,21 +14,40 @@ PRESENTATIONSFILES='../presentations/*.md'
 INDEXFILE="../index/index.md"
 
 echo '# All files' > $INDEXFILE
+echo "" >> $INDEXFILE
 
 echo '## Plans' >> $INDEXFILE
+echo "" >> $INDEXFILE
 for file in $PLANSFILES
 do
   echo "- [`basename $file .md`](./`basename $file .md`.html)" >> $INDEXFILE
 done
+echo "" >> $INDEXFILE
 
 echo '## Presentations' >> $INDEXFILE
+echo "" >> $INDEXFILE
 for file in $PRESENTATIONSFILES
 do
   echo "- [`basename $file .md`](./`basename $file .md`.html)" >> $INDEXFILE
 done
+echo "" >> $INDEXFILE
 
 echo '## Notes' >> $INDEXFILE
+echo "" >> $INDEXFILE
 for file in $NOTESFILES
 do
   echo "- [`basename $file .md`](./`basename $file .md`.html)" >> $INDEXFILE
 done
+echo "" >> $INDEXFILE
+
+cd ..
+REPO=`pwd`
+cd tools
+
+echo '## Versions' >> $INDEXFILE
+echo "" >> $INDEXFILE
+git log --pretty=format:"- [%ai |%an |%s]"\
+"(https://cdn.rawgit.com/$GITHUBUSER/`basename $REPO`/%H/)" >> $INDEXFILE
+echo "" >> $INDEXFILE
+echo "- [bleeding edge a.k.a. master]\
+(https://rawgit.com/$GITHUBUSER/`basename $REPO`/master/)" >> $INDEXFILE
