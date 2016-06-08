700ecefe549c8199c7030dba516a7c8c80065f78
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-06-08 10:23:45 +0200

    insertfromfile function

--- tools/processmdfiles.sh
+++ tools/processmdfiles.sh
@@ -5,6 +5,24 @@
 # sverre.stikbakke@ntnu.no 18.04.2016
 #
 
+insertfromfile () {
+  #
+  # ${1}: placeholder token
+  # ${2}: replacement content file
+  # ${3}: file to edit
+  #
+  # sed [-n] program [file-list]
+  # sed program syntax
+  #   -e edit command follows (part of program)
+  #   / /  address - regex to select line(s) to edit
+  #  {} grouping of commands
+  #  -r read command - read file content
+  #  -d delete - do not print original line content
+  #  -i edit file in place
+  #
+  sed -e "/${1}/{" -e "r ${2}" -e "d" -e "}" -i "${3}"
+}
+
 mkdir -p "${WORK}"
 
 for file in ${MDFILES}
@@ -25,12 +43,10 @@ do
    -i "${WORK}/temp.html"
 
   # insert css code into html file
-  sed -e "/${PLACEHOLDERCSS}/{" -e "r ${STYLES}/${CSS}" -e "d" -e "}"\
-   -i "${WORK}/temp.html"
+  insertfromfile "${PLACEHOLDERCSS}" "${STYLES}/${CSS}" "${WORK}/temp.html"
 
   # insert modified markdown file into html file
-  sed -e "/${PLACEHOLDERMD}/{" -e "r ${WORK}/temp.md" -e "d" -e "}"\
-   -i "${WORK}/temp.html"
+  insertfromfile "${PLACEHOLDERMD}" "${WORK}/temp.md" "${WORK}/temp.html"
 
   # rename html file to md file name and move to output directory
   echo "${HTMLOUTPUT}/$(basename ${file} .md).html"
